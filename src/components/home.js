1166f4498441e3dc4d2359fe68eb2d359bfa556f
Josh Genereux <jgenereux@stratacache.com>
2023-09-14 15:24:55 -0400

    started work on new portfolio

---
+++ src/components/home.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div className="home">
+        <header className="header">
+          <div className="header__name">Josh Genereux</div>
+          <div className="header__title">Software Developer</div>
+        </header>
+
+        <div className="body">
+          <div>
+            
+          </div>
+        </div>
+    </div>
+  );
+}
+
+export default Home;
