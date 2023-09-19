e648ae69a623711a09b3a263046518bd64ad1cad
Josh Genereux <jgenereux@stratacache.com>
2023-09-19 09:50:30 -0400

    working on portfolio

--- src/components/techFlipper.js
+++ src/components/techFlipper.js
@@ -1,14 +1,13 @@
 import React from "react";
 
 const TechFlipper = ({props}) => {
-  console.log(props)
   const name = props[0][0];
   const asset = props[0][1];
 
   return(
     <div className="tech-flipper">  
       <h1 className="tech-name">{name}</h1>
-      <img className={`tech ${props[1]}`} src={asset} alt=""/>
+      <img className={`tech ${name}`} src={asset} alt=""/>
     </div>
   )
 };
--- src/containers/Info.js
+++ src/containers/Info.js
@@ -1,16 +1,11 @@
 import React from "react";
 import TechStack from "../components/TechStack";
-import pic from '../assets/facepic.jpeg';
 import aboutMe from '../texts/aboutMe.json';
 
 const Info = () => {
 
   return (
     <div className="body__info">
-      <div className="body__info__pic">
-        <img className="pic" src={pic} alt="" /> 
-      </div>
-
       <div className="body__info__info">
         <h1 className="aboutme-header">About Me</h1>
         <div className="aboutme-text">{aboutMe.about}</div>
