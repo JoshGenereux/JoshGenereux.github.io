179fc0d4a4de566c7dfdbde65976adae0b839893
Josh Genereux <jgenereux@stratacache.com>
2023-09-19 14:42:18 -0400

    working, removed README

--- src/components/TechStack.js
+++ src/components/TechStack.js
@@ -7,7 +7,7 @@ const TechStack = () => {
 
   return (
     <div className="body__info__tech">  
-      <h1 className="tech-header">Tech Stack</h1>
+      <h1 className="tech-header">My Tech Stack</h1>
       <div className="tech-stack-container">
         {techObj.map((t, i) => (
           <TechFlipper props={[t, i]} key={i} />
--- src/containers/Info.js
+++ src/containers/Info.js
@@ -1,12 +1,16 @@
 import React from "react";
 import TechStack from "../components/TechStack";
 import aboutMe from '../texts/aboutMe.json';
+import pic from '../assets/facepic.jpeg';
 
 const Info = () => {
 
   return (
     <div className="body__info">
       <div className="body__info__info">
+        <div className="info-pic-container">
+          <img className="info-pic" src={pic} alt="" />
+        </div>
         <h1 className="aboutme-header">About Me</h1>
         <div className="aboutme-text">{aboutMe.about}</div>
       </div>
