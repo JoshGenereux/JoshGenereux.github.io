7e521cc43736d051ff02acf3ca20afdf29e95b7c
Josh Genereux <jgenereux@stratacache.com>
2023-09-27 16:59:04 -0400

    added projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -9,13 +9,13 @@ import marsSurface from '../../assets/marssurface.jpeg';
 
 const Projects = () => {
   return (
-    <div className={styles.projects}>
+    <div className={styles.projects} id="projects">
       <div className={styles.current}>current</div>
       <div className={styles.header}>PROJECTS</div>
       <div className={styles.container}>
         <div className={styles.box}>
           <img src={earth} alt="" className={styles.planet} />
-          <h1>JPass</h1>
+          <h1 className={styles.jpass}>JPass</h1>
           <ul>
             <li>React</li>
             <li>Javascript</li>
@@ -31,18 +31,17 @@ const Projects = () => {
         </div>
         <div className={styles.box}>
           <img src={mars} alt="" className={styles.planet} />
-          <h1>JPass</h1>
+          <h1>Live Fitness</h1>
           <ul>
-            <li>React</li>
+            <li>HTML</li>
+            <li>css</li>
             <li>Javascript</li>
-            <li>Sequelize</li>
-            <li>Node</li>
           </ul>
           <img src={marsSurface} alt="" className={styles.beach} />
           <p>
-            Jpass is a password manager, built using React.js, Javascript, CSS,
-            HTML and SQL. Storing all of the passowords in a SQL database, and
-            utilizing Sequelize.js in the back end to send the information over.
+            Live Fitness, is a fitness tracker and workout app, that stores all
+            of your workouts on a SQL database. You can create a workout each
+            day, and pick from hundreds of different exercises from a list.
           </p>
         </div>
         <div className={styles.box}>
@@ -51,18 +50,18 @@ const Projects = () => {
             alt=""
             className={`${styles.planet} ${styles.moon}`}
           />
-          <h1>JPass</h1>
+          <h1>Weather</h1>
           <ul>
             <li>React</li>
             <li>Javascript</li>
-            <li>Sequelize</li>
+            <li>API</li>
             <li>Node</li>
           </ul>
           <img src={moonSurface} alt="" className={styles.beach} />
           <p>
-            Jpass is a password manager, built using React.js, Javascript, CSS,
-            HTML and SQL. Storing all of the passowords in a SQL database, and
-            utilizing Sequelize.js in the back end to send the information over.
+            A simple weather app, hooked up to an API. you can log in and search
+            any location in the worls, and the api will make a call and send you
+            back the information.
           </p>
         </div>
       </div>
