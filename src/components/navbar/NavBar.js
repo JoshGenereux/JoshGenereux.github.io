22ebef4c104bce5d174ee2e5776951d1ffb7b081
Josh Genereux <jgenereux@stratacache.com>
2023-09-26 21:45:56 -0400

    redoing website

--- src/components/navbar/NavBar.js
+++ src/components/navbar/NavBar.js
@@ -1,71 +1,15 @@
-import styles from './NavBar.module.scss';
+import React from 'react';
+import styles from './navbar.module.scss';
 
 const NavBar = () => {
   return (
-    <nav className={styles.navContainer}>
-      <ul>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('home')
-              .scrollTo({ top: 0, behavior: 'smooth' })
-          }
-        >
-          Josh
-        </li>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('info')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          About
-        </li>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('techStack')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Tech-Stack
-        </li>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('projects')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Projects
-        </li>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('techStack')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Resume
-        </li>
-        <li
-          className={styles.navLink}
-          onClick={() =>
-            document
-              .getElementById('techStack')
-              .scrollIntoView({ behavior: 'smooth' })
-          }
-        >
-          Contact
-        </li>
-      </ul>
-    </nav>
+    <div className={styles.navbar}>
+      <div className={styles.button}>About</div>
+      <div className={styles.button}>spaced</div>
+      <div className={styles.button}>Josh</div>
+      <div className={styles.button}>Projects</div>
+      <div className={styles.button}>Contact</div>
+    </div>
   );
 };
 
