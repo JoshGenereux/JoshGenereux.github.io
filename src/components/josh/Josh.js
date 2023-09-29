26beec7a4c7c81a4d1f0ec5da847486cb4e9afb3
Josh Genereux <jgenereux@stratacache.com>
2023-09-29 10:23:49 -0400

    got state working for about me

--- src/components/josh/Josh.js
+++ src/components/josh/Josh.js
@@ -2,33 +2,51 @@ import React from 'react';
 import styles from './josh.module.scss';
 import SideBorder from '../sideBorder/SideBorder';
 import { useState } from 'react';
-import texts from '../../texts/aboutMe.json';
-import hobbies from '../../texts/hobbies.json';
+import { about, hobbies, past } from '../../texts/about';
 
 const Josh = () => {
-  const [header, setHeader] = useState(texts.header);
-  const [subHeader, setSubHeader] = useState(texts.subheader);
-  const [about, setAbout] = useState(texts.about);
-  const [top, setTop] = useState(texts.top);
-  const [bottom, setBottom] = useState(texts.bottom);
-  const [top2, setTop2] = useState(texts.top2);
-  const [bottom2, setBottom2] = useState(texts.bottom2);
-  const [top3, setTop3] = useState(texts.top3);
-  const [bottom3, setBottom3] = useState(texts.bottom3);
-  const [description, setDescription] = useState(texts.description);
-
+  const [aboutMe, setAbout] = useState({
+    ...about,
+  });
+  const [index, setIndex] = useState(1);
   const switchNext = (e) => {
     e.preventDefault();
-    setHeader(hobbies.header);
-    setSubHeader(hobbies.subheader);
-    setAbout(hobbies.about);
-    setTop(hobbies.top);
-    setTop2(hobbies.top2);
-    setTop3(hobbies.top3);
-    setBottom(hobbies.bottom);
-    setBottom2(hobbies.bottom2);
-    setBottom3(hobbies.bottom3);
-    setDescription(hobbies.description);
+    const forward = e.target.id === 'forward';
+    console.log(forward);
+    let count = index;
+
+    switch (count) {
+      case 1:
+        if (forward) {
+          setAbout({ ...hobbies });
+          setIndex(2);
+        } else {
+          setAbout({ ...past });
+          setIndex(3);
+        }
+        break;
+      case 2:
+        if (forward) {
+          setAbout({ ...past });
+          setIndex(3);
+        } else {
+          setAbout({ ...about });
+          setIndex(1);
+        }
+        break;
+      case 3:
+        if (forward) {
+          setAbout({ ...about });
+          setIndex(1);
+        }
+        setAbout({ ...hobbies });
+        setIndex(2);
+        break;
+      default:
+        setAbout({ ...about });
+        setIndex(1);
+        return;
+    }
   };
 
   return (
@@ -38,34 +56,40 @@ const Josh = () => {
         <div className={styles.picCover}></div>
       </div>
       <div className={styles.aboutContainer}>
-        <div className={styles.headerDescript}>{subHeader}</div>
-        <div className={styles.header}>{header}</div>
+        <div className={styles.headerDescript}>{aboutMe.subheader}</div>
+        <div className={styles.header}>{aboutMe.header}</div>
         <div className={styles.clickerContainer}>
           <div className={`${styles.aboutClick}`}></div>
           <div className={`${styles.hobbiesClick}`}></div>
           <div className={`${styles.schoolClick}`}></div>
         </div>
         <div className={styles.textContainer}>
-          <div className={styles.aboutText}>{about}</div>
+          <div className={styles.aboutText}>{aboutMe.about}</div>
         </div>
         <div className={styles.containers}>
           <div className={styles.container}>
-            <span className={styles.top}>{top}</span>
-            <span className={styles.bottom}>{bottom}</span>
+            <span className={styles.top}>{aboutMe.top}</span>
+            <span className={styles.bottom}>{aboutMe.bottom}</span>
           </div>
           <div className={styles.container}>
-            <span className={styles.top}>{top2}</span>
-            <span className={styles.bottom}>{bottom2}</span>
+            <span className={styles.top}>{aboutMe.top2}</span>
+            <span className={styles.bottom}>{aboutMe.bottom2}</span>
           </div>
           <div className={styles.container}>
-            <span className={styles.top}>{top3}</span>
-            <span className={styles.bottom}>{bottom3}</span>
+            <span className={styles.top}>{aboutMe.top3}</span>
+            <span className={styles.bottom}>{aboutMe.bottom3}</span>
           </div>
         </div>
         <div className={styles.buttonContainer}>
-          <button className={styles.leftButton}>{'>'}</button>
-          <div className={styles.centerButton}>{description}</div>
-          <button className={styles.rightButton} onClick={switchNext}>
+          <button className={styles.leftButton} id="back" onClick={switchNext}>
+            {'>'}
+          </button>
+          <div className={styles.centerButton}>{aboutMe.description}</div>
+          <button
+            className={styles.rightButton}
+            id="forward"
+            onClick={switchNext}
+          >
             {'>'}
           </button>
         </div>
---
+++ src/texts/about.js
@@ -0,0 +1,43 @@
+const about = {
+  about:
+    "Hi, I'm Josh. I was raised mostly in Utah where I attended DevMountain Web Development Bootcamp. Afterwards, I became a Software Developer at Stratacache in Dayton, Ohio.",
+  header: 'Josh Genereux',
+  subheader: 'About Me',
+  top: 'Syracuse',
+  bottom: 'Utah',
+  top2: 'Dayton',
+  bottom2: 'Ohio',
+  top3: 'Junior',
+  bottom3: 'Developer',
+  description: 'Learn More',
+};
+
+const hobbies = {
+  about:
+    'I love coding, computers, and video games. I enjoy hiking and being in nature. I also like working on my car, riding motorcycles, and tending to my house plants.',
+  header: 'Hobbies',
+  subheader: 'What I Do',
+  top: 'Video Games',
+  bottom: 'Coding',
+  top2: 'Cars',
+  bottom2: 'Audi',
+  top3: 'Nature',
+  bottom3: 'Hiking',
+  description: 'Learn More',
+};
+
+const past = {
+  about:
+    'After graduating high school, I worked construction for many years. My brother introduced me to Java, leading me down the path to become a Software Developer.',
+  header: 'Past Life',
+  subheader: 'What I Did',
+  top: 'Construction',
+  bottom: 'Road Work',
+  top2: 'Java',
+  bottom2: 'DevMountain',
+  top3: 'Programming',
+  bottom3: 'Career',
+  description: 'Learn More',
+};
+
+export { about, hobbies, past };
