1606a69dfcc2f43c5d985592261a454360956b32
Josh Genereux <jgenereux@stratacache.com>
2023-09-29 12:12:40 -0400

    working on rebuilding projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -4,6 +4,7 @@ import earth from '../../assets/earth.png';
 import mars from '../../assets/marspic.png';
 import SideBorder from '../sideBorder/SideBorder';
 import jupitor from '../../assets/jupiter.png';
+import Jpass from './modals/jpass/Jpass';
 
 const Projects = () => {
   return (
@@ -11,7 +12,27 @@ const Projects = () => {
       <SideBorder down={'josh'} />
       <div className={styles.current}>current</div>
       <div className={styles.header}>PROJECTS</div>
-      <div className={styles.container}></div>
+      <div className={styles.pickerContainer}>
+        <div className={styles.pick}>
+          <span className={styles.pickName}>JPass</span>
+          <div className={styles.pickUnderline}></div>
+        </div>
+        <div className={styles.pick}>
+          <span className={styles.pickName}>Live Fitness</span>
+          <div className={styles.pickUnderline}></div>
+        </div>
+        <div className={styles.pick}>
+          <span className={styles.pickName}>Weather</span>
+          <div className={styles.pickUnderline}></div>
+        </div>
+        <div className={styles.pick}>
+          <span className={styles.pickName}>Portfolio</span>
+          <div className={styles.pickUnderline}></div>
+        </div>
+      </div>
+      <div className={styles.container}>
+        <Jpass />
+      </div>
       <div className={styles.more}>More to come</div>
     </div>
   );
---
+++ src/components/projects/modals/jpass/Jpass.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import styles from './jpass.module.scss';
+import mars from '../../../../assets/marspic.png';
+
+const Jpass = () => {
+  return (
+    <div className={styles.jpass}>
+      <div></div>
+    </div>
+  );
+};
+
+export default Jpass;
