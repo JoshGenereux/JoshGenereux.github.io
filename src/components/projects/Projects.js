0d6542b89ec6ce44cf9d3da3c9335344025342f3
Josh Genereux <jgenereux@stratacache.com>
2023-10-02 16:02:43 -0400

    working on state for projects

--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,37 +1,128 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import styles from './projects.module.scss';
 import earth from '../../assets/earth.png';
 import mars from '../../assets/marspic.png';
 import SideBorder from '../sideBorder/SideBorder';
 import jupitor from '../../assets/jupiter.png';
-import Jpass from './modals/jpass/Jpass';
+import moon from '../../assets/moon.png';
+import sun from '../../assets/sun.png';
+import purple from '../../assets/purple.png';
+import { jpass, fitness } from './texts';
 
 const Projects = () => {
+  const namesArr = [
+    'JPass',
+    'Live Fitness',
+    'Weather',
+    'Portfolio',
+    'Calorie Tracker',
+  ];
+
+  const [showProject, setShowProject] = useState({
+    jpassState: false,
+    fitnessState: false,
+  });
+
+  const switchTo = (e) => {
+    e.preventDefault();
+    const element = e.target.id;
+    let text = '';
+    if (element === 'jupiter') {
+      setShowProject({ ...showProject });
+      setShowProject({ jpassState: true });
+      if (showProject.jpassState && element === 'jupiter') text = jpass;
+    }
+    if (element === 'mars') {
+      setShowProject({ ...showProject });
+      setShowProject({ fitnessState: true });
+      if (showProject.fitnessState && element === 'mars') text = fitness;
+    }
+
+    const header = document.getElementById('header');
+    const description = document.getElementById('description');
+    const top1 = document.getElementById('top1');
+    const top2 = document.getElementById('top2');
+    const top3 = document.getElementById('top3');
+    const bottom1 = document.getElementById('bottom1');
+    const bottom2 = document.getElementById('bottom2');
+    const bottom3 = document.getElementById('bottom3');
+
+    header.textContent = text.header;
+    description.textContent = text.description;
+    top1.textContent = text.top1;
+    top2.textContent = text.top2;
+    top3.textContent = text.top3;
+    bottom1.textContent = text.bottom1;
+    bottom2.textContent = text.bottom2;
+    bottom3.textContent = text.bottom3;
+  };
+
   return (
     <div className={styles.projects} id="projects">
       <SideBorder down={'josh'} />
       <div className={styles.current}>current</div>
-      <div className={styles.header}>PROJECTS</div>
+      <div className={styles.projectsHeader}>PROJECTS</div>
       <div className={styles.pickerContainer}>
-        <div className={styles.pick}>
-          <span className={styles.pickName}>JPass</span>
-          <div className={styles.pickUnderline}></div>
-        </div>
-        <div className={styles.pick}>
-          <span className={styles.pickName}>Live Fitness</span>
-          <div className={styles.pickUnderline}></div>
-        </div>
-        <div className={styles.pick}>
-          <span className={styles.pickName}>Weather</span>
-          <div className={styles.pickUnderline}></div>
-        </div>
-        <div className={styles.pick}>
-          <span className={styles.pickName}>Portfolio</span>
-          <div className={styles.pickUnderline}></div>
-        </div>
+        {namesArr.map((name, i) => (
+          <div className={styles.pick} key={i}>
+            <span className={styles.pickName}>{name}</span>
+            <div className={styles.pickUnderline}></div>
+          </div>
+        ))}
       </div>
       <div className={styles.container}>
-        <Jpass />
+        <div className={styles.jpass}>
+          <div className={styles.infoContainer}>
+            <div className={styles.header} id="header"></div>
+            <div className={styles.about} id="description"></div>
+            <div className={styles.boxContainer}>
+              <div className={styles.box}>
+                <span className={styles.number} id="top1"></span>
+                <span className={styles.skill} id="bottom1"></span>
+              </div>
+              <div className={styles.box}>
+                <span className={styles.number} id="top2"></span>
+                <span className={styles.skill} id="bottom2"></span>
+              </div>
+              <div className={styles.box}>
+                <span className={styles.number} id="top3"></span>
+                <span className={styles.skill} id="bottom3"></span>
+              </div>
+            </div>
+          </div>
+          <div className={styles.picContainer}>
+            <img
+              className={`${styles.planet} ${styles.planet__one}`}
+              src={mars}
+              alt=""
+              id="mars"
+              onClick={switchTo}
+            />
+            <img
+              className={`${styles.planet} ${styles.planet__two}`}
+              src={earth}
+              alt=""
+            />
+            <img className={styles.sun} src={sun} alt="" />
+            <img
+              className={`${styles.planet} ${styles.planet__three}`}
+              src={moon}
+              alt=""
+            />
+            <img
+              className={`${styles.planet} ${styles.planet__four}`}
+              src={jupitor}
+              alt=""
+              id="jupiter"
+              onClick={switchTo}
+            />
+            <img
+              className={`${styles.planet} ${styles.planet__five}`}
+              src={purple}
+              alt=""
+            />
+          </div>
+        </div>
       </div>
       <div className={styles.more}>More to come</div>
     </div>
---
+++ src/components/projects/texts.js
@@ -0,0 +1,25 @@
+const jpass = {
+  header: 'JPass',
+  description:
+    'JPass is a password manager. Built using React and Javascript, it stores and manages all of your passwords on a Heroku database, utilizing a SQL database.',
+  top1: '56%',
+  bottom1: 'Javascript',
+  top2: '36%',
+  bottom2: 'React',
+  top3: '56%',
+  bottom3: 'HTML',
+};
+
+const fitness = {
+  header: 'Live Fitness',
+  description:
+    'Live Fitness is a work out builder and tracker app, but using only vanilla javascript, HTML, and CSS. It utilizes a Heroku database to store information and completed workouts.',
+  top1: '56%',
+  bottom1: 'Javascript',
+  top2: '36%',
+  bottom2: 'React',
+  top3: '56%',
+  bottom3: 'HTML',
+};
+
+export { jpass, fitness };
