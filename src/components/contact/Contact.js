01ba54d23df659091525ed9871b38c6cd40bdc7b
Josh Genereux <jgenereux@stratacache.com>
2023-10-04 19:16:33 -0400

    working on contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,16 +1,32 @@
 import React from 'react';
 import styles from './contact.module.scss';
 import SideBorder from '../sideBorder/SideBorder';
-import mars from '../../assets/marspic.png';
 
 const Contact = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    console.log('submit');
+  };
+
   return (
     <div className={styles.contact} id="contact">
       <SideBorder down="" />
       <div className={styles.header}>Contact Me</div>
-      <div className={styles.picContainer}>
-        <img src={mars} alt="" className={styles.planet} />
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div className={styles.nameContainer}>
+          <input id="name" type="text" className={styles.nameInput} />
+        </div>
+        <div className={styles.emailContainer}>
+          <input id="email" type="text" className={styles.emailInput} />
+        </div>
+        <div className={styles.textContainer}>
+          <textarea id="text" typet="text" className={styles.textInput} />
+        </div>
+        <button type="submit" className={styles.button}>
+          submit
+        </button>
+      </form>
     </div>
   );
 };
