eb4e9b38eb37bf4bf615db40a30c9672bba4e927
Josh Genereux <jgenereux@stratacache.com>
2023-10-05 15:54:24 -0400

    finished styling contact form

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -1,12 +1,18 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import styles from './contact.module.scss';
 import SideBorder from '../sideBorder/SideBorder';
 
 const Contact = () => {
+  const email = useRef();
+  const name = useRef();
+  const text = useRef();
+
   const handleSubmit = (e) => {
     e.preventDefault();
 
-    alert('email sent, Thank you!');
+    const nameValue = name.current.value;
+    const emailValue = email.current.value;
+    const textValue = text.current.value;
   };
 
   return (
@@ -20,6 +26,7 @@ const Contact = () => {
             type="text"
             className={styles.nameInput}
             placeholder="name"
+            ref={name}
           />
         </div>
         <div className={styles.emailContainer}>
@@ -28,6 +35,7 @@ const Contact = () => {
             type="text"
             className={styles.emailInput}
             placeholder="email"
+            ref={email}
           />
         </div>
         <div className={styles.textContainer}>
@@ -36,6 +44,7 @@ const Contact = () => {
             typet="text"
             className={styles.textInput}
             placeholder="your message here"
+            ref={text}
           />
         </div>
         <button type="submit" className={styles.button}>
