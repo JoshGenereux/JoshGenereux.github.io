dba49288141920ebf05d7c668b19f6b3dbd7300c
Josh Genereux <jgenereux@stratacache.com>
2023-10-05 15:11:36 -0400

    working on input form styling

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -15,13 +15,28 @@ const Contact = () => {
       <div className={styles.header}>Contact Me</div>
       <form onSubmit={handleSubmit}>
         <div className={styles.nameContainer}>
-          <input id="name" type="text" className={styles.nameInput} />
+          <input
+            id="name"
+            type="text"
+            className={styles.nameInput}
+            placeholder="name"
+          />
         </div>
         <div className={styles.emailContainer}>
-          <input id="email" type="text" className={styles.emailInput} />
+          <input
+            id="email"
+            type="text"
+            className={styles.emailInput}
+            placeholder="email"
+          />
         </div>
         <div className={styles.textContainer}>
-          <textarea id="text" typet="text" className={styles.textInput} />
+          <textarea
+            id="text"
+            typet="text"
+            className={styles.textInput}
+            placeholder="your message here"
+          />
         </div>
         <button type="submit" className={styles.button}>
           submit
